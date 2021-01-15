0b6cc2e81fa1338beb9afe6427efaf19bdbaa5ac
Chris Cheetham <chris@cheetham.com>
2021-01-15 14:48:26 -0500

    Initial import

---
+++ SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+    }
+}
